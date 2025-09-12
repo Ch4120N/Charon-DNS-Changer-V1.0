3c041e185108e8697c3805af90091c24199ee1bc
Ch4120N <charonsecagency@gmail.com>
2025-09-12 16:29:34 +0430

    Updated utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -5,3 +5,7 @@ import sys
 def clear_screen():
     os.system('cls') if (sys.platform.lower() ==
                          "win32") else os.system('clear')
+
+def colorizeInput(text:str):
+    print(text, end='', flush=True)
+    return input()
