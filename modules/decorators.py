8a15570305a0c6a06552c8ebc9c504992667b2f2
Ch4120N <charonsecagency@gmail.com>
2025-09-12 18:03:46 +0430

    Updated decorators.py

--- modules/decorators.py
+++ modules/decorators.py
@@ -1,14 +1,26 @@
 import sys
 from modules.banner import BRACKETS_COLOR, FOREGROUND_RESET, CUSTOM_COLOR, CUSTOM_TEXT_COLOR, TEXTS_COLOR, DEFAULT_SUB_BANNER_COLOR
 
-def SUCCESS(message:str):
-    sys.stdout.write(f'{BRACKETS_COLOR}[ {CUSTOM_TEXT_COLOR}+ {BRACKETS_COLOR}]{CUSTOM_TEXT_COLOR}' + message + f'\n{FOREGROUND_RESET}')
+
+def SUCCESS(message: str):
+    sys.stdout.write(
+        f'{BRACKETS_COLOR}[ {CUSTOM_TEXT_COLOR}+ {BRACKETS_COLOR}]{CUSTOM_TEXT_COLOR} ' +
+        message + f'\n{FOREGROUND_RESET}'
+    )
     sys.stdout.flush()
 
-def ERROR(message:str):
-    sys.stdout.write(f'{BRACKETS_COLOR}[ {TEXTS_COLOR}- {BRACKETS_COLOR}]{TEXTS_COLOR}' + message + f'\n{FOREGROUND_RESET}')
+
+def ERROR(message: str):
+    sys.stdout.write(
+        f'{BRACKETS_COLOR}[ {TEXTS_COLOR}- {BRACKETS_COLOR}]{TEXTS_COLOR} ' +
+        message + f'\n{FOREGROUND_RESET}'
+    )
     sys.stdout.flush()
 
-def INFO(message:str):
-    sys.stdout.write(f'{BRACKETS_COLOR}[ {CUSTOM_COLOR}# {BRACKETS_COLOR}]{DEFAULT_SUB_BANNER_COLOR}' + message + f'\n{FOREGROUND_RESET}')
+
+def INFO(message: str):
+    sys.stdout.write(
+        f'{BRACKETS_COLOR}[ {CUSTOM_COLOR}# {BRACKETS_COLOR}]{DEFAULT_SUB_BANNER_COLOR} ' +
+        message + f'\n{FOREGROUND_RESET}'
+    )
     sys.stdout.flush()
