6305161902780b604b10df6238c62de30d92611c
Ch4120N <charonsecagency@gmail.com>
2025-09-13 05:20:00 +0430

    Updated scripts/launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,7 +1,7 @@
-export CHDNSCHANGER="/opt/Charon-DNS-Changer-V1.0"
+export CHDNSCHANGER="/opt/Charon-DNS-Changer"
 
 if [[ $1 == '-h' || $1 == 'help' ]]; then
-	echo "To run \`Charon DNS Changer\` type \`chdnschanger\` in your prompt"
+	echo "To run \`Charon DNS Changer\` type \`ChDNSChanger\` in your prompt"
 	echo
 
 else
