9658a32d6e8da92fb5fcde13b20e935030b96f54
Ch4120N <charonsecagency@gmail.com>
2025-09-13 05:01:29 +0430

    Updated modules/decorators.py

--- modules/decorators.py
+++ modules/decorators.py
@@ -1,16 +1,43 @@
 import sys
-from modules.banner import BRACKETS_COLOR, FOREGROUND_RESET, CUSTOM_COLOR, NUMBERS_COLOR, CUSTOM_TEXT_COLOR, TEXTS_COLOR, DEFAULT_SUB_BANNER_COLOR, INPUTS_COLOR
+from modules.banner import (
+    BRACKETS_COLOR,       # Color for brackets in messages
+    FOREGROUND_RESET,     # Resets text color to terminal default
+    CUSTOM_COLOR,         # Custom accent color for info messages
+    NUMBERS_COLOR,        # Color for numeric indicators / warnings
+    CUSTOM_TEXT_COLOR,    # Color for success messages
+    TEXTS_COLOR,          # Color for error messages
+    DEFAULT_SUB_BANNER_COLOR,  # Color for standard message text
+    INPUTS_COLOR          # Color for input prompts
+)
 
 
 def SUCCESS(message: str):
+    """
+    Prints a success message to the terminal with colorized prefix.
+    
+    Format: [ + ] message
+    Colors:
+        + sign: CUSTOM_TEXT_COLOR
+        brackets: BRACKETS_COLOR
+        message: CUSTOM_TEXT_COLOR
+    """
     sys.stdout.write(
         f'{BRACKETS_COLOR}[ {CUSTOM_TEXT_COLOR}+ {BRACKETS_COLOR}]{CUSTOM_TEXT_COLOR} ' +
-        message + f'\n{FOREGROUND_RESET}'
+        message + f'\n{FOREGROUND_RESET}'  # Reset colors after message
     )
-    sys.stdout.flush()
+    sys.stdout.flush()  # Ensure message is immediately displayed
 
 
 def ERROR(message: str):
+    """
+    Prints an error message to the terminal with colorized prefix.
+    
+    Format: [ - ] message
+    Colors:
+        - sign: TEXTS_COLOR (red)
+        brackets: BRACKETS_COLOR
+        message: TEXTS_COLOR
+    """
     sys.stdout.write(
         f'{BRACKETS_COLOR}[ {TEXTS_COLOR}- {BRACKETS_COLOR}]{TEXTS_COLOR} ' +
         message + f'\n{FOREGROUND_RESET}'
@@ -19,20 +46,52 @@ def ERROR(message: str):
 
 
 def INFO(message: str):
+    """
+    Prints an informational message to the terminal with colorized prefix.
+    
+    Format: [ # ] message
+    Colors:
+        # sign: CUSTOM_COLOR (cyan)
+        brackets: BRACKETS_COLOR
+        message: DEFAULT_SUB_BANNER_COLOR (white)
+    """
     sys.stdout.write(
         f'{BRACKETS_COLOR}[ {CUSTOM_COLOR}# {BRACKETS_COLOR}]{DEFAULT_SUB_BANNER_COLOR} ' +
         message + f'\n{FOREGROUND_RESET}'
     )
     sys.stdout.flush()
 
-def WARNING(message:str):
+
+def WARNING(message: str):
+    """
+    Prints a warning message to the terminal with colorized prefix.
+    
+    Format: [ ! ] message
+    Colors:
+        ! sign: NUMBERS_COLOR (yellow)
+        brackets: BRACKETS_COLOR
+        message: NUMBERS_COLOR
+    """
     sys.stdout.write(
         f'{BRACKETS_COLOR}[ {NUMBERS_COLOR}! {BRACKETS_COLOR}]{NUMBERS_COLOR} ' +
         message + f'\n{FOREGROUND_RESET}'
     )
     sys.stdout.flush()
 
+
 def INPUT(message: str):
+    """
+    Generates a colorized input prompt for user interaction.
+    
+    Format: [ > ] message
+    Colors:
+        > sign: INPUTS_COLOR (magenta)
+        brackets: BRACKETS_COLOR
+        message: DEFAULT_SUB_BANNER_COLOR (white)
+    
+    Returns:
+        str: Formatted prompt ready to pass to input() function
+    """
     return (
         f'{BRACKETS_COLOR}[ {INPUTS_COLOR}> {BRACKETS_COLOR}]{DEFAULT_SUB_BANNER_COLOR} ' +
         message + FOREGROUND_RESET
